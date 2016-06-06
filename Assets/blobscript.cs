eef66a65ce6415b2cff7a70151539db4f4036931
Andy Thomson <andy@andythomason.com>
2016-06-06 19:16:30 +0100

    add the project

---
+++ Assets/blobscript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blobscript : MonoBehaviour {
+
+    void OnMouseDrag() {
+        // calculate the mouse position in world space.
+        float camera_distance = 10;
+        Vector2 mp = Input.mousePosition;
+        Camera cam = GameObject.FindObjectOfType<Camera>();
+        Vector3 mouse_pos = cam.ScreenToWorldPoint(new Vector3(mp.x, mp.y, camera_distance));
+        Vector3 delta = mouse_pos - transform.position;
+
+        // move the blob towards the mouse.
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        float spring_constant = 100;
+        rb.AddForce(new Vector2(delta.x * spring_constant, delta.y * spring_constant));
+    }
+}
---
+++ Assets/springs.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class springs : MonoBehaviour {
+    public List<GameObject> balls;
+    public List<float> natural_length;
+    public float spring_constant;
+    public float damping_constant;
+
+    void Start() {
+        for (int i = 0; i < balls.Count; ++i) {
+            for (int j = i+1; j < balls.Count; ++j) {
+                Rigidbody2D rb1 = balls[i].GetComponent<Rigidbody2D>();
+                Rigidbody2D rb2 = balls[j].GetComponent<Rigidbody2D>();
+                float d = (rb1.transform.position - rb2.transform.position).magnitude;
+                natural_length.Add(d);
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        int nli = 0;
+        for (int i = 0; i < balls.Count; ++i) {
+            for (int j = i+1; j < balls.Count; ++j) {
+                Rigidbody2D rb1 = balls[i].GetComponent<Rigidbody2D>();
+                Rigidbody2D rb2 = balls[j].GetComponent<Rigidbody2D>();
+                //float deltav = (rb1.velocity - rb2.velocity).magnitude;
+                Vector3 delta = rb1.transform.position - rb2.transform.position;
+                Vector3 dir = delta.normalized;
+                float d = delta.magnitude;
+                float f = (d - natural_length[nli++]) * spring_constant;
+                rb1.AddForce(new Vector2(-f * dir.x, -f * dir.y));
+                rb2.AddForce(new Vector2(f * dir.x, f * dir.y));
+            }
+        }
+    }
+}
